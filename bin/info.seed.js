81633920a38571bee54833879e78cdde203345e5
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-02-11 22:22:17 +0100

    new folders and info

---
+++ bin/info.seed.js
@@ -0,0 +1,7 @@
+require('../db/index.js')
+
+const AnimeModel = require('../models/Anime.model')
+
+const axios = require('axios')
+const mongoose = require('mongoose')
+
---
+++ models/Anime.model.js
@@ -0,0 +1,12 @@
+const mongoose= require ('mongoose')
+
+let AnimeSchema = new mongoose.Schema({
+  title: String,
+  image_url: String,
+  synopsis: String,
+  episodes: Number
+})
+
+let AnimeModel = mongoose.model('animeinfo', AnimeSchema)
+
+module.exports = AnimeModel
