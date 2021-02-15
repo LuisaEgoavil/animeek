5d904c3f19f235a04726aa6195abf4482b8ac2f3
Megumikawa <megumi19901016@gmail.com>
2021-02-15 23:42:39 +0100

    add css

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,16 @@ document.addEventListener(
 //-----index page slider-----//
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
+
+// Instantiate a slider
+var mySlider = new Slider("input.slider", {
+	// initial options object
+});
+
+// Call a method on the slider
+var value = mySlider.getValue();
+
+// For non-getter methods, you can chain together commands
+mySlider
+	.setValue(5)
+	.setValue(7);
